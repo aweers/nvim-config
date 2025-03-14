c1784969fc815db46769b0221299acfeb2196730
Alexander Weers <mail@aweers.de>
2025-03-14 16:10:04 +0100

    Resolve conflict of flash and mini.surround

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -120,8 +120,8 @@ require("lazy").setup({
 		opts = {},
 		-- stylua: ignore
 		keys = {
-			{ "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-			{ "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+			{ "<CR>", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+			{ "<leader><CR>", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
 			{ "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
 			{ "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
 			{ "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
